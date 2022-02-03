8f867dab5156b397ec4a8c2272c9adb3ebd49af4
pniyongabo <patrickniyongabo@gmail.com>
2022-02-03 13:28:02 -0800

    add initial node files

---
+++ character.js
@@ -0,0 +1,3 @@
+const character = { first: "Leia", last: "Organa" };
+
+module.exports = character;
---
+++ server.js
@@ -0,0 +1,4 @@
+const character = require("./character");
+
+console.log("node example");
+console.log(`${character.first}`);
